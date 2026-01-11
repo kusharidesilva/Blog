53cb7250880477717080c870099be119f3faa84a
Kushari Desilva <bsse0122011570@saegis.ac.lk>
2026-01-11 12:08:55 +0530

    Update copyright year to the current year in footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
         {/* Copyright */}
         <div className="border-t border-gray-800 mt-12 pt-8 text-center">
           <p className="text-gray-400 text-sm">
-            © BlogNest 2025 by Kushari Desilva | All Right Reserved
+            © BlogNest {new Date().getFullYear()} by Kushari Desilva | All Right Reserved
           </p>
         </div>
         
